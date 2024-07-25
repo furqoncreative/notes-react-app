04758166607f6c718e84fb6241f0739f978412e2
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2024-07-25 11:36:58 +0700

    add navigation

--- src/components/NotesAppHeader.jsx
+++ src/components/NotesAppHeader.jsx
@@ -1,7 +1,15 @@
+import {Link} from "react-router-dom";
+
 function NotesAppHeader() {
     return (
         <header className="header-container">
             <h1 className="header-title">Notes App</h1>
+            <nav className="navigation">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/add-note">Add Note</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
