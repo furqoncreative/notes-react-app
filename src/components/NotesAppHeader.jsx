712b9c3148b6ed99f0c1a334704241ac8911bfc7
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2024-08-12 15:59:35 +0700

    add logout icon

--- src/components/NotesAppHeader.jsx
+++ src/components/NotesAppHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { HiOutlineHome, HiOutlinePlusCircle } from "react-icons/hi2";
 import PropTypes from "prop-types";
+import { HiOutlineLogout } from "react-icons/hi";
 
 function NotesAppHeader({ authedUser }) {
   return (
@@ -22,6 +23,14 @@ function NotesAppHeader({ authedUser }) {
               <Link className="nav-item" to="/add-note">
                 <HiOutlinePlusCircle />
               </Link>
+            </li>
+            <li>
+              <div className="logout-nav-item">
+                <span className="nav-item">{authedUser.name}</span>
+                <Link className="nav-item" to="/add-note">
+                  <HiOutlineLogout />
+                </Link>
+              </div>
             </li>
           </ul>
         </nav>
